d09f69b6478eb3e08651d8a80d50e4bc73ca2112
Сенаторов Иван <senatorov.5000@gmail.com>
2022-03-19 13:50:31 +0300

    finished

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Post
-
 from .models import Group
+
+from .models import Post
 
 DATE = ('-pub_date')[:10]
 
 
 def index(request):
-    posts = Post.objects.order_by('DATE')
+    posts = Post.objects.order_by(DATE)
     context = {
         'posts': posts,
     }
@@ -18,7 +18,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
 
-    posts = Post.objects.filter(group=group).order_by('DATE')
+    posts = Post.objects.filter(group=group).order_by(DATE)
     context = {
         'group': group,
         'posts': posts,
