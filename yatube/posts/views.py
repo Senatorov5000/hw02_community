feebba9790fe0fd22c9d1e68f2b22646253a4ca9
Сенаторов Иван <senatorov.5000@gmail.com>
2022-03-23 18:04:09 +0300

    fixing

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,7 +6,7 @@ DATE = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')
+    posts = Post.objects.order_by('-pub_date')[:DATE]
     context = {
         'posts': posts,
     }
@@ -14,8 +14,8 @@ def index(request):
 
 
 def group_posts(request, slug):
-    group = get_object_or_404(Group, slug=slug),
-    posts = Post.objects.filter(group=group).order_by('-pub_date')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:DATE]
     context = {
         'group': group,
         'posts': posts,
